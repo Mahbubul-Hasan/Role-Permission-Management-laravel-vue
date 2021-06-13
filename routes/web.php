fd3803651d9f9307e7755eef95f00f74f0c22032
Mahbubul Hasan <mahbubul161@gmail.com>
2021-06-13 10:42:36 +0600

    Install laravel UI

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Backend\DashboardController;
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
