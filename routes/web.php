fe9937e73e41e2bcdb19221d3e7f6d6d34795ec8
Mahbubul Hasan <mahbubul161@gmail.com>
2021-06-14 10:52:01 +0600

    Install & setup spatie package

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Backend\DashboardController;
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+    // Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
 });
